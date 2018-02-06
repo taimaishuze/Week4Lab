2bf5171f68c2e5064e3f393852885ed990cc9249
taimaishuze <35374281+taimaishuze@users.noreply.github.com>
2018-02-05 21:09:01 -0600

    Fixed dd command line

--- Chapt4script.sh
+++ Chapt4script.sh
@@ -6,28 +6,36 @@ clear
 
 # This command will update and upgrade debian before beginning new scripts
 sudo apt-get update && sudo apt-get upgrade
+echo ""
 
 # Change directory to /dev
 echo "Changing directory to /dev"
+echo ""
 cd /dev
 
 # Detailed list of directory contents
 echo "Directory contents"
+echo ""
 ls -l
 
 # Change directory to /proc and view partitions
 echo "Next, changing directory to /proc and take a look at the partitions"
-cd /proc && cat /proc/partition
+echo ""
+cd /proc && cat /proc/partitions
 
 # Using example from textbook pg 48 to demonstrate dd command.  This should copy data from a block /dev/zero
 # to a new file labeled new_file
 echo "Using dd example to create new_file"
+echo ""
+cd
 dd if=/dev/zero of=new_file bs=1024 count=1
 echo "You should see new_file listed below"
+echo ""
 ls
 
 # Remove new_file
 echo "Now to remove new_file"
+echo ""
 rm new_file
 ls
 echo "new_file removed"
